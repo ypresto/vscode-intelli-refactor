24cc5b8ca64d09b642706964ecf014b21e60676b
Yuya Tanaka <yuya.presto@gmail.com>
2021-01-11 23:52:08 +0900

    Fix "Invalid argument 'kind'" error

--- src/candidateNode.ts
+++ src/candidateNode.ts
@@ -170,7 +170,7 @@ export async function fetchCodeActions(
       'vscode.executeCodeActionProvider',
       uri,
       selection,
-      filter.kind
+      filter.kind?.value
     )) || [];
 
   // reset selection
